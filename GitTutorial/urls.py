fe315bbead11d781cba200043eaa6ff7e5672420
Marek Arendt <marek.arendt@yougov.com>
2019-09-22 13:33:02 +0200

    master view get url

--- GitTutorial/urls.py
+++ GitTutorial/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from simple_views.views import MasterView
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', MasterView.as_view(), name='master-view')
 ]
