bd9bc2e78dfa94fa89f3ff998b758a27343695df
Marek Arendt <marek.arendt@yougov.com>
2019-09-24 09:45:24 +0200

    - master refactor 1

--- GitTutorial/urls.py
+++ GitTutorial/urls.py
@@ -20,6 +20,6 @@ import simple_views.views as simple_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', simple_views.MasterView.as_view(), name='master-view'),
+    path('', simple_views.MasterView.as_view(), name='masterview'),
     path('test', simple_views.TestView.as_view(), name='test_viewwwww'),
 ]
